83ba88ca3bb6f973347e1c448ad262e449eac11d
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-14 11:48:24 -0400

    misc

---
+++ src/main/java/jikjii/menu/item/InMemoryItemRepository.java
@@ -0,0 +1,8 @@
+package jikjii.menu.item;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InMemoryItemRepository extends CrudRepository<Item, Long> {
+}
